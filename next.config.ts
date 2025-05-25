0bc5ca3955da45f9f5d203d8d767a7e1cf04c1d1
Bilo Lwabona <bilo.lwabona@gmail.com>
2025-05-25 18:46:44 +0200

    chore: test next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import withPWA from 'next-pwa'
+import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const isDev = process.env.NODE_ENV === 'development'
 
-export default nextConfig;
+const baseConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+      allowedOrigins: [
+        'https://visionairy.vercel.app',
+        'https://aida-prod.vercel.app',
+        'https://localhost:3000',
+      ],
+    },
+  },
+}
+
+// Only PWA-specific config goes here
+const withPwa = withPWA({
+  dest: 'public',
+  disable: isDev, // disable SW during development
+  register: true,
+  skipWaiting: true,
+})
+
+// Export the final config
+export default withPwa(baseConfig)
